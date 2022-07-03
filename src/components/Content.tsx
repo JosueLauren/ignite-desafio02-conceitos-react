1519f632e4d6f69802f2ea7f3a74a36c81830c8b
Josue Laurentino <josue.laurentino@csptecnologia.com>
2022-07-03 17:47:54 -0300

    app dividido entre Content e SideBar

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,30 @@
-export function Content() {
+import { MovieCard } from './MovieCard';
+
+import {MovieProps} from '../App'
+
+import '../styles/content.scss';
+
+interface ContentProps {
+  movies: MovieProps[],
+  titleGenre: string
+}
+
+export function Content({movies, titleGenre}:ContentProps) {
   // Complete aqui
+
+  return (
+    <div className="container">
+      <header>
+        <span className="category">Categoria:<span> {titleGenre}</span></span>
+      </header>
+
+      <main>
+        <div className="movies-list">
+          {movies.map(movie => (
+            <MovieCard key ={movie.imdbID} title={movie.Title} poster={movie.Poster} runtime={movie.Runtime} rating={movie.Ratings[0].Value} />
+          ))}
+        </div>
+      </main>
+    </div>
+  )
 }
